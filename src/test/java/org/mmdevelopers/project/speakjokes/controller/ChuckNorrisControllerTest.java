03a0983924b12261fbc97a1d777204a47704e751
Magdalena Makowska <magda.mgs@gmail.com>
2023-11-23 19:09:02 +0100

    add test

--- src/test/java/org/mmdevelopers/project/speakjokes/controller/ChuckNorrisControllerTest.java
+++ src/test/java/org/mmdevelopers/project/speakjokes/controller/ChuckNorrisControllerTest.java
@@ -1,7 +1,22 @@
 package org.mmdevelopers.project.speakjokes.controller;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mmdevelopers.project.speakjokes.api.chucknorrisjokes.ChuckNorrisJokesApiResponse;
+import org.mmdevelopers.project.speakjokes.service.ChuckNorrisJokesService;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChuckNorrisControllerTest {
 
+    @Test
+    void randomJoke() {
+        //given
+        ChuckNorrisJokesService chuckNorrisJokesService = new ChuckNorrisJokesService();
+        ChuckNorrisController chuckNorrisController = new ChuckNorrisController(chuckNorrisJokesService);
+        //when
+        ChuckNorrisJokesApiResponse chuckNorrisJokesApiResponse = chuckNorrisController.randomJoke();
+        //then
+        Assertions.assertNotNull(chuckNorrisJokesApiResponse, "response is not null");
+    }
 }
