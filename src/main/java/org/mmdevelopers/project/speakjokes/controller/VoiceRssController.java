96f6cb261b6e3f93abdfb15babdfa575c1d0b5de
Magdalena Makowska <magda.mgs@gmail.com>
2023-12-04 17:30:47 +0100

    add new method to controller

--- src/main/java/org/mmdevelopers/project/speakjokes/controller/VoiceRssController.java
+++ src/main/java/org/mmdevelopers/project/speakjokes/controller/VoiceRssController.java
@@ -1,7 +1,22 @@
 package org.mmdevelopers.project.speakjokes.controller;
 
+import org.mmdevelopers.project.speakjokes.service.VoiceRssService;
+
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class VoiceRssController {
     private static final Logger LOGGER = Logger.getLogger(VoiceRssController.class.getName());
+    private final VoiceRssService voiceRssService;
+
+    public VoiceRssController(VoiceRssService voiceRssService) {
+        this.voiceRssService = voiceRssService;
+    }
+
+    public boolean play(String textToVoice) throws IOException {
+        LOGGER.info("play: " + textToVoice);
+        boolean spoken = voiceRssService.speakJoke(textToVoice);
+        LOGGER.info("play: " + spoken);
+        return spoken;
+    }
 }
