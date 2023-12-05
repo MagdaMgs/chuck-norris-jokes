6ef68beff119a5ab3fff26181817a7cfc61352f7
Magdalena Makowska <magda.mgs@gmail.com>
2023-12-05 20:34:30 +0100

    add constructor

--- src/main/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeService.java
+++ src/main/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeService.java
@@ -4,4 +4,12 @@ import java.util.logging.Logger;
 
 public class SpeakJokeService {
     private static final Logger LOGGER = Logger.getLogger(AudioPlayService.class.getName());
+
+    private final ChuckNorrisJokesService chuckNorrisJokesService;
+    private final VoiceRssService voiceRssService;
+
+    public SpeakJokeService(ChuckNorrisJokesService chuckNorrisJokesService, VoiceRssService voiceRssService) {
+        this.chuckNorrisJokesService = chuckNorrisJokesService;
+        this.voiceRssService = voiceRssService;
+    }
 }
