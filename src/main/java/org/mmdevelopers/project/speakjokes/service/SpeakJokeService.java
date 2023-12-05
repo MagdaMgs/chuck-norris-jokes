cdac280a47aec2b8a448871166f59a3cb723bc67
Magdalena Makowska <magda.mgs@gmail.com>
2023-12-05 20:32:35 +0100

    Add logger

--- src/main/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeService.java
+++ src/main/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeService.java
@@ -1,4 +1,7 @@
 package org.mmdevelopers.project.speakjokes.service;
 
+import java.util.logging.Logger;
+
 public class SpeakJokeService {
+    private static final Logger LOGGER = Logger.getLogger(AudioPlayService.class.getName());
 }
