fcbf345dabb8474ac43c293862422b57324a11cf
Magdalena Makowska <magda.mgs@gmail.com>
2023-12-06 20:42:49 +0100

    new method

--- src/main/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeService.java
+++ src/main/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeService.java
@@ -1,15 +1,21 @@
 package org.mmdevelopers.project.speakjokes.service;
 
+import org.mmdevelopers.project.speakjokes.api.chucknorrisjokes.ChuckNorrisJokesApiResponse;
+
 import java.util.logging.Logger;
 
 public class SpeakJokeService {
     private static final Logger LOGGER = Logger.getLogger(AudioPlayService.class.getName());
-
     private final ChuckNorrisJokesService chuckNorrisJokesService;
     private final VoiceRssService voiceRssService;
 
     public SpeakJokeService(ChuckNorrisJokesService chuckNorrisJokesService, VoiceRssService voiceRssService) {
         this.chuckNorrisJokesService = chuckNorrisJokesService;
         this.voiceRssService = voiceRssService;
+    }
+
+    public void speakJoke(){
+        ChuckNorrisJokesApiResponse chuckNorrisJokesApiResponse = chuckNorrisJokesService.randomJoke();
+        
     }
 }
