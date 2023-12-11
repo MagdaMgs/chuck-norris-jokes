11759edb4ecf3bbce258144b2f7c14ab8346641c
Magdalena Makowska <magda.mgs@gmail.com>
2023-12-11 15:45:44 +0100

    add unit test

--- src/test/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeServiceTest.java
+++ src/test/java/org/mmdevelopers/project/speakjokes/service/SpeakJokeServiceTest.java
@@ -1,16 +1,24 @@
 package org.mmdevelopers.project.speakjokes.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpeakJokeServiceTest {
 
     @Test
-    void speakJoke() {
+    void speakJoke() throws IOException {
         //given
-
+        AudioPlayService audioPlayService = new AudioPlayService();
+        VoiceRssService voiceRssService = new VoiceRssService(audioPlayService);
+        ChuckNorrisJokesService chuckNorrisJokesService = new ChuckNorrisJokesService();
+        SpeakJokeService speakJokeService = new SpeakJokeService(chuckNorrisJokesService, voiceRssService);
         //when
+        boolean spoken = speakJokeService.speakJoke();
         //then
+        Assertions.assertTrue(spoken, "Joke not spoken");
     }
 }
