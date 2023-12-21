926954ada17cd5d53bf41782b3a80ce2e417363b
Magdalena Makowska <magda.mgs@gmail.com>
2023-12-21 21:40:10 +0100

    add spring unit test

---
+++ src/test/java/org/mmdevelopers/project/speakjokes/service/ChuckNorrisJokesServiceSpringTest.java
@@ -0,0 +1,12 @@
+package org.mmdevelopers.project.speakjokes.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChuckNorrisJokesServiceSpringTest {
+
+    @Test
+    void randomJoke() {
+    }
+}
